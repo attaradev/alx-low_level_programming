67740b5e8ddeafe8a5b9a3c6267edfb02e9d2fba
Mike Attara <mpyebattara@gmail.com>
2022-09-09 22:36:04 +0000

    print without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (!(i == 'q' || i == 'e'))
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
