e6cfcab77a5bfc198cd1a3a2d6204c070d8f97b8
Mike Attara <mpyebattara@gmail.com>
2022-09-09 23:10:49 +0000

    print combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = 48; y <= 57; y++)
+		{
+			if (!(x == y || y <= x))
+			{
+				putchar(x);
+				putchar(y);
+				if (!(x == 56 && y == 57))
+				{
+					putchar(44);
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
