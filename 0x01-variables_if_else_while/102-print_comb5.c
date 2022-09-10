68e2cc55924fcfc1aa830918ea81c73b0e856503
Mike Attara <mpyebattara@gmail.com>
2022-09-10 01:01:14 +0000

    fix comma

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -29,11 +29,11 @@ int main(void)
 						putchar(' ');
 						putchar(k);
 						putchar(l);
-					}
-					if (!(i == 57 && j == 56 && k == 57 && l == 57))
-					{
-						putchar(',');
-						putchar(' ');
+						if (!(i == 57 && j == 56 && k == 57 && l == 57))
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 					l++;
 				}
