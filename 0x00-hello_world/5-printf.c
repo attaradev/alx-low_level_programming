bdc1e064f5bcb30250f3e937bdda222b6915e49f
Mike Attara <mpyebattara@gmail.com>
2022-09-10 03:30:02 +0000

    style: betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints exactly 'with proper grammar, but the outcome is a piece of art,', 
+ * main - prints exactly 'with proper grammar, but the outcome is a piece of art,',
  *        followed by a new line
  * Return: Always 0 (Success)
  */
