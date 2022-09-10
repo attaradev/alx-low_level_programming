94f64df19dbb176eca7444cc4076df432dc86b3e
Mike Attara <mpyebattara@gmail.com>
2022-09-10 03:28:45 +0000

    style: betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly 'and that piece of art is useful" - Dora Korpar, 2015-10-19', 
+ * main - prints exactly 
+ *        'and that piece of art is useful" - Dora Korpar, 2015-10-19',
  *        followed by a new line, to the standard error
  * Return: Always 0 (Success)
  */
