1219dfcf8c5fbfb949628ce2b5a26aab714d86ec
Mike Attara <mpyebattara@gmail.com>
2022-09-13 13:03:36 +0000

    refactor

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,4 @@
-#include <stdio.h>
-
-/**
- * print_alphabet - print alphabets
- * Return: void
- */
-void print_alphabet(void)
-{
-	for (char c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-  putchar('\n');
-}
+#include "main.h"
 
 /**
  * print_alphabets_x10 - print alphabets 10 times
@@ -19,18 +6,8 @@ void print_alphabet(void)
  */
 void print_alphabets_x10(void)
 {
-  for (int i = 0; i < 10; i++)
-  {
-    print_alphabet();
-  }
-}
-
-/**
- * main - entry point
- * Return: Always 0
- */
-int main(void)
-{
-	print_alphabets_x10();
-	return 0;
+	for (int i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
 }
