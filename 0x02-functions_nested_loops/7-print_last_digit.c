2ee3bb25df39ac1c13dfc0c0ae1cfba5e98806e5
Mike Attara <mpyebattara@gmail.com>
2022-09-13 19:59:43 +0000

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,6 +14,6 @@ int print_last_digit(int n)
 	last_digit = n % 10;
 	if (last_digit < 0)
 		last_digit = -last_digit;
-	_putchar(last_digit);
+	_putchar('0' + last_digit);
 	return (last_digit);
 }
