77e02946e38ff59d87400e85e33a236a61a85e96
Mike Attara <mpyebattara@gmail.com>
2022-09-13 22:23:00 +0000

    first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * main - prints out first 98
+ * fibonacci suit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long x, y, z;
+	unsigned long ax, bx, ay, by;
+	unsigned long a, b;
+
+	x = 0;
+	y = 1;
+	for (i = 0; i < 92; i++)
+	{
+		z = x + y;
+		printf("%lu, ", z);
+		x = y;
+		y = z;
+	}
+	ax = x / 10000000000;
+	ay = y / 10000000000;
+	bx = x % 10000000000;
+	by = y % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		a = ax + ay;
+		b = bx + by;
+		if ((bx + by) > 9999999999)
+		{
+			a += 1;
+			b %= 10000000000;
+		}
+		printf("%lu%lu", a, b);
+		if (i != 98)
+			printf(", ");
+
+		ax = ay;
+		bx = by;
+		ay = a;
+		by = b;
+	}
+
+	printf("\n");
+
+	return (0);
+}
