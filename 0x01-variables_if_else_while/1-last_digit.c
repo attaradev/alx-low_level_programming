926976bd0a32084c3dcd1114ed9bc4f84ec83114
Mike Attara <mpyebattara@gmail.com>
2022-09-13 23:43:10 +0000

    cleanup

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - print the last digit of the number stored in the variable n
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
