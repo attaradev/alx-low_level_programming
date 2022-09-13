578ca0f8e6c4e8e147f2e7e78fb3de6779f575db
Mike Attara <mpyebattara@gmail.com>
2022-09-13 14:10:10 +0000

    check alpha character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isalpha - checks for alpha character
+ *
+ * @c: character to check
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
