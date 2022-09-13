206792115703a7a401309d4888d77ad384a631c6
Mike Attara <mpyebattara@gmail.com>
2022-09-13 22:11:57 +0000

    print sum of fabonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even fibonacci
+ * suit elements fibonacci suit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long x, y, z, sum;
+
+	x = 0;
+	y = 1;
+	z = 0;
+	sum = 0;
+
+	while (z <= 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+
+		if ((x % 2) == 0)
+			sum += x;
+	}
+
+	printf("%ld\n", sum);
+
+	return (0);
+}
