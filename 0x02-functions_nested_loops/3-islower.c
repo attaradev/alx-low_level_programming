307c677d1380f905618c592433a8cdd0378e1f94
Mike Attara <mpyebattara@gmail.com>
2022-09-13 13:55:40 +0000

    check lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,15 @@
 
 /**
  * _islower - checks for lowercase character
+ *
+ * @c: character to check
+ *
  * Return: 1 or 0
  */
 int _islower(int c)
 {
-	
+	int result;
+
+	result = 'a' <= c <= 'z' ? 0 : 1;
+	return (result);
 }
