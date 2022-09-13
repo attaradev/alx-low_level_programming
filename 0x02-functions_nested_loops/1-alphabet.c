1b8ed2c2d232ee076ae6b3b8530ee198568c4c8e
Mike Attara <mpyebattara@gmail.com>
2022-09-13 13:00:08 +0000

    refactor

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,19 +8,9 @@ void print_alphabet(void)
 {
 	char x;
 
-	for (x = 97; x <= 122; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		_putchar(x);
 	}
 	_putchar('\n');
-}
-
-/**
- * main - prints alphabets and newline
- * Return: Always 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
