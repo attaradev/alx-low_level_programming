72a87c25aacca16587646c6466ed86403e9c9f17
Mike Attara <mpyebattara@gmail.com>
2022-09-15 16:10:30 -0700

    refactor

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,21 @@
  */
 int main(void)
 {
-  int number;
+	int number;
 
-  for (number = 1; number <= 100; number++)
+	for (number = 1; number <= 100; number++)
+	{
+		if ((number % 3 == 0) && (number % 5 == 0))
+			printf("FizzBuzz ");
+		else if (number % 3 == 0)
+			printf("Fizz ");
+		else if (number % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", number);
+	}
 
-    if ((number % 3 == 0) && (number % 5 == 0))
-      printf("FizzBuzz ");
-    else if (number % 3 == 0)
-      printf("Fizz ");
-    else if (number % 5 == 0)
-    {
-      if (number != 100)
-        printf("Buzz ");
-      else
-        printf("Buzz");
-    }
-    else
-      printf("%d ", number);
+	printf("\n");
 
-  printf("\n");
-
-  return (0);
+	return (0);
 }
