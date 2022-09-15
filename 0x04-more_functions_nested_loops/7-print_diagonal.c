36b48c4d3d8e0d45ce0f50936c7e5860b4340675
Mike Attara <mpyebattara@gmail.com>
2022-09-15 15:23:54 -0700

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,15 +17,11 @@ void print_diagonal(int n)
 
 			for (space = 0; space < len; space++)
 				_putchar(' ');
-
 			_putchar('\\');	
-
 			if (len == (n - 1))
 				continue;
-
 			_putchar('\n');
 		}
 	}
-
 	_putchar('\n');
 }
