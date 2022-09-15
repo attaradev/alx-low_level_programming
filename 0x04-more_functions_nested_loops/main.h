79661643557ef6e32bad32623b1913638953dfca
Mike Attara <mpyebattara@gmail.com>
2022-09-15 07:31:28 +0000

    add issuper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+/**
+ * File: main.h
+ * Author: Mike Attara
+ * Desc: Header file containing function declarations
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int);
+
+#endif /* MAIN_H */
