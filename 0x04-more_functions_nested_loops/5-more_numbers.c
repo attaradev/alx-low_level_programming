3011bf152faafb135da40900844df8761c0054e0
Mike Attara <mpyebattara@gmail.com>
2022-09-15 14:53:57 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,13 @@ void more_numbers(void)
 	for (count = 1; count <= 10; count++)
 	{
 		int number;
-		
+
 		for (number = 0; number <= 14; number++)
 		{
 			if (number > 9)
 				_putchar('0' + (number / 10));
-
 			_putchar('0' + (number % 10));
 		}
-
 		_putchar('\n');
 	}
 }
