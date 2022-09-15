2772a102de0859a31663b324e46731dcfdcab584
Mike Attara <mpyebattara@gmail.com>
2022-09-15 07:43:59 +0000

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+  int number;
+
+  for (number = 48; number < 58; number++)
+  {
+    if ((number == 50) || (number == 52))
+    {
+      continue;
+    }
+
+    _putchar(number);
+  }
+
+  _putchar('\n');
+}
