fc08be4720af6b0a7e3a98f79c3d7527fcea8e19
Mike Attara <mpyebattara@gmail.com>
2022-09-15 16:20:04 -0700

    remive final space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,16 @@ int main(void)
 	for (number = 1; number <= 100; number++)
 	{
 		if ((number % 3 == 0) && (number % 5 == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (number % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (number % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", number);
+			printf("%d", number);
+
+		if (number !== 100)
+			printf(" ");
 	}
 
 	return (0);
