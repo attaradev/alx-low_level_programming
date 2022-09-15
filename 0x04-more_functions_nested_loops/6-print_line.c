c2f0cf09ec12c092913301575383c93a3e18aa8b
Mike Attara <mpyebattara@gmail.com>
2022-09-15 14:58:20 -0700

    refactor

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,15 +2,16 @@
 
 /**
  * print_line - prints a straight line
- * @n:parameter
+ * @n: parameter
  * Return: void
  */
 void print_line(int n)
 {
-  while (n-- > 0)
-  {
-    _putchar('_');
-  }
-
-  _putchar('\n');
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	
+	_putchar('\n');
 }
