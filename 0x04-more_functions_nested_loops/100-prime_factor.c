019e0939476d4646d6750dab7579d1d0cb123db5
Mike Attara <mpyebattara@gmail.com>
2022-09-15 16:57:43 -0700

    fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,25 +6,25 @@
  */
 int main(void)
 {
-  long number = 612852475143;
-  int inc;
+	long number = 612852475143;
+	int inc;
 
-  while (inc++ < number / 2)
-  {
-    if (number % inc == 0)
-    {
-      number /= 2;
-      continue;
-    }
+	while (inc++ < number / 2)
+	{
+		if (number % inc == 0)
+		{
+			number /= 2;
+			continue;
+		}
 
-    for (inc = 3; inc < number / 2; inc += 2)
-    {
-      if (number % inc == 0)
-        number /= inc;
-    }
-  }
+		for (inc = 3; inc < number / 2; inc += 2)
+		{
+			if (number % inc == 0)
+				number /= inc;
+		}
+	}
 
-  printf("%ld\n", number);
+	printf("%ld\n", number);
 
-  return (0);
+	return (0);
 }
