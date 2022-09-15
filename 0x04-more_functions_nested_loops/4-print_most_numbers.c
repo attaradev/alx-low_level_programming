1afac6e694144395f7696b415b716deb7860f681
Mike Attara <mpyebattara@gmail.com>
2022-09-15 14:37:36 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 	int number;
 	
-	for (number = 48; number < 58; number+)
+	for (number = 48; number < 58; number++)
 	{
-		if (!((number == 50) || (number == 52)))
+		if ((number != 50) && (number != 52))
 			_putchar(number);
 	}
-	
+
 	_putchar('\n');
 }
