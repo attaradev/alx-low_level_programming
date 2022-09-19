efd7c3a5e3008d2eaa2284b41fe3137a973328d6
Mike Attara <mpyebattara@gmail.com>
2022-09-19 21:14:18 +0000

    add swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
