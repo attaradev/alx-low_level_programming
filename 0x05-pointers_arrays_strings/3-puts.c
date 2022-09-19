bc387152631789dcd874361d066e5e8d660a4bbd
Mike Attara <mpyebattara@gmail.com>
2022-09-19 14:37:43 -0700

    add puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ * @s: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while(s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,5 +10,7 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _putchar(char c);
+void _puts(char *str);
 
 #endif /* MAIN_H */
