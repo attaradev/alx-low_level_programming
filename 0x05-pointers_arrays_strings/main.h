c864f33f6531c17e63527033749abcf27e5eba6e
Mike Attara <mpyebattara@gmail.com>
2022-09-19 21:01:38 +0000

    add main header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+/**
+ * File: main.h
+ * Author: Mike Attara
+ * Desc: Header file containing function declarations
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
