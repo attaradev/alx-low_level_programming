a8f27e3d9749d8758b61bd70208a8d1a755a2496
Mike Attara <mpyebattara@gmail.com>
2022-09-19 19:07:37 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,8 +16,10 @@ int _atoi(char *s)
 	do {
 		if (*s == '-')
 			i *= -1;
-		else if (*s >= 0 && *s <= 9)
+		else if (*s >= '0' && *s <= '9')
 			num = num * 10 + (*s + '0')
+		else if (*s > 0)
+			break;
 	} while (*s++);
 
 	return (num * i);
