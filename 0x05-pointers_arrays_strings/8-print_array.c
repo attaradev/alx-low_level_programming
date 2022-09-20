87096f69d3d030b9443a8df785de7d995fcef926
Mike Attara <mpyebattara@gmail.com>
2022-09-19 18:23:55 -0700

    refactor

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		char str[] = i == n - 1 ? "%d\n" : "%d, ";
-		printf(str, a[i]);
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
 		i++;
 	}
 }
