5e93a2a38106fd548fc6bc70d823926c742b3514
Mike Attara <mpyebattara@gmail.com>
2022-09-21 13:58:52 -0700

    refactor

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,16 @@
 char *leet(char *str)
 {
 	int x, y;
-	char leet[8];
-       
-	leet[0] = 'O';
-	leet[1] = 'L';
-	leet[2] = '?';
-	leet[3] = 'E';
-	leet[4] = 'A';
-	leet[5] = '?';
-	leet[6] = '?';
-	leet[7] = 'T';
+	char leet[5][2] = {{'A', '4'}, {'E', '3'}, {'O', '0'}, {'T', '7'}, {'L', '1'}};
+
 	x = 0;
 
 	while (str[++x])
 	{
 		for (y = 0; y < 8; y++)
 		{
-			if (str[x] == leet[y] || str[x] - 32 == leet[y])
-				str[x] = '0' + y;
+			if (str[x] == leet[y][0] || str[x] - 32 == leet[y][0])
+				str[x] = leet[y][1];
 		}
 	}
 
