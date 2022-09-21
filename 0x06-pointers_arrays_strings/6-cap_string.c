e3189290fb68f81bebcd176f04de758463f12707
Mike Attara <mpyebattara@gmail.com>
2022-09-21 20:12:28 +0000

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,12 @@ char *cap_string(char *str)
 
 	while (str[++i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-
 		char c;
+
+		c = str[i];
+
+		while (!(c >= 'a' && c <= 'z'))
+			i++;
 
 		c = str[i - 1];
 		if (c == ' ' ||
