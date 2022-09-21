1b2174f610e2a8170fc0463f0afa17e709fdc730
Mike Attara <mpyebattara@gmail.com>
2022-09-21 11:52:59 -0700

    change to uppercase

---
+++ 0x06-pointers_arrays_strings/6-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - change lowercase letters to uppercase
+ * @s: string
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		char c;
+
+		c = s[i];
+		if (c >= 'a' && c <= 'z')
+			s[i] = c + 32;
+	}
+
+	return (s);
+}
