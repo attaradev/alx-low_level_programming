f741653842a83ab72386e04356d171913f52b40d
Mike Attara <mpyebattara@gmail.com>
2022-09-21 21:47:24 +0000

    rename var

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,15 @@
 char *leet(char *str)
 {
 	int x, y;
-	char leet[5][2] = {{'A', '4'}, {'E', '3'}, {'O', '0'}, {'T', '7'}, {'L', '1'}};
+	char lt[5][2] = {{'A', '4'}, {'E', '3'}, {'O', '0'}, {'T', '7'}, {'L', '1'}};
 
 	x = 0;
 	while (str[x] != '\0')
 	{
 		for (y = 0; y < 5; y++)
 		{
-			if (str[x] == leet[y][0] || str[x] - 32 == leet[y][0])
-				str[x] = leet[y][1];
+			if (str[x] == lt[y][0] || str[x] - 32 == lt[y][0])
+				str[x] = lt[y][1];
 		}
 
 		x++;
