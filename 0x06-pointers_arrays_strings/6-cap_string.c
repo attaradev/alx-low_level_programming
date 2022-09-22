eca4d33fbd9ca4b945ca87c7235cbb165ce3c390
Mike Attara <mpyebattara@gmail.com>
2022-09-22 16:25:06 -0700

    refactor

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,15 @@ char *cap_string(char *str)
 
 	i = 0;
 
-	while (str[++i])
+	while (str[i])
 	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+			i++;
+			continue;
+		}
+
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
@@ -30,6 +37,8 @@ char *cap_string(char *str)
 				str[i - 1] == '{' ||
 				str[i - 1] == '}')
 			str[i] -= 32;
+
+		i++;
 	}
 
 	return (str);
