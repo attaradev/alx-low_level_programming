cca4cfc74a7d9792ec511a58222761c6080b0829
Mike Attara <mpyebattara@gmail.com>
2022-09-22 16:14:46 -0700

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,27 +7,30 @@
  */
 char *cap_string(char *str)
 {
-		int index = 0;
+	int i;
 
-			while (str[++index])
-					{
-								while (!(str[index] >= 'a' && str[index] <= 'z'))
-												index++;
+	i = 0;
 
-										if (str[index - 1] == ' ' ||
-																str[index - 1] == '\t' ||
-																				str[index - 1] == '\n' ||
-																								str[index - 1] == ',' ||
-																												str[index - 1] == ';' ||
-																																str[index - 1] == '.' ||
-																																				str[index - 1] == '!' ||
-																																								str[index - 1] == '?' ||
-																																												str[index - 1] == '"' ||
-																																																str[index - 1] == '(' ||
-																																																				str[index - 1] == ')' ||
-																																																								str[index - 1] == '{' ||
-																																																												str[index - 1] == '}')
-														str[index] -= 32;
-											}
-				return (str);
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}')
+			str[i] -= 32;
+	}
+
+	return (str);
 }
