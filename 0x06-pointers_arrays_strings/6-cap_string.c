8ae33d638fd752ddbf41ab35668bfa2ce68f00d3
Mike Attara <mpyebattara@gmail.com>
2022-09-22 16:12:36 -0700

    refactor

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,30 +7,27 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+		int index = 0;
 
-	i = 0;
+			while (str[++index])
+					{
+								while (!(str[index] >= 'a' && str[index] <= 'z'))
+												index++;
 
-	while (str[++i])
-	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}')
-			str[i] -= 32;
-	}
-
-	return (str);
+										if (str[index - 1] == ' ' ||
+																str[index - 1] == '\t' ||
+																				str[index - 1] == '\n' ||
+																								str[index - 1] == ',' ||
+																												str[index - 1] == ';' ||
+																																str[index - 1] == '.' ||
+																																				str[index - 1] == '!' ||
+																																								str[index - 1] == '?' ||
+																																												str[index - 1] == '"' ||
+																																																str[index - 1] == '(' ||
+																																																				str[index - 1] == ')' ||
+																																																								str[index - 1] == '{' ||
+																																																												str[index - 1] == '}')
+														str[index] -= 32;
+											}
+				return (str);
 }
