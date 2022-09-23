cb14d2541b26f912d78b163a0851a7d24d5c50d5
Mike Attara <mpyebattara@gmail.com>
2022-09-23 01:16:06 +0000

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - print integer
+ * @n: number
+ * Return: void
+ */
+void print_number(int n)
+{
+  unsigned int j, i, base_10 = 1;
+
+  if (n < 0)
+  {
+    _putchar('-');
+    i = -n;
+  }
+  else
+  {
+    i = n;
+  }
+  j = i;
+
+  while (j > 9)
+  {
+    j /= 10;
+    base_10 *= 10;
+  }
+
+  j = i;
+  while (base_10 > 1)
+  {
+    _putchar((j / base_10) + '0');
+    j %= base_10;
+    base_10 /= 10;
+  }
+  _putchar((i % 10) + '0');
+}
