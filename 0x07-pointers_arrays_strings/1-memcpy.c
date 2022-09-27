d8f3bb8421c4fc81fec97cbeb9f785066bd346ad
Mike Attara <mpyebattara@gmail.com>
2022-09-27 00:27:58 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: amount of memory to copy
+ * Return: copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@
 #define HEADER_FILE
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* HEADER_FILE*/
