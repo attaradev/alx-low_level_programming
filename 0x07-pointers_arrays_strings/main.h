b6b13a3d408f021fc5cf556f140eb79caaf567b6
Mike Attara <mpyebattara@gmail.com>
2022-09-27 00:25:53 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+/**
+ * File: main.h
+ * Author: Mike Attara
+ * Desc: Header file for function declarations
+ */
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* HEADER_FILE*/
