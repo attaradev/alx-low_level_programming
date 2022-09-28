0beb5cbb79301db11f7d3296f2bab1c5db3ea4f6
Mike Attara <mpyebattara@gmail.com>
2022-09-28 23:42:08 +0000

    refactor

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,22 +7,12 @@
  */
 int factorial(int n)
 {
-	int res;
-
 	if (n < 0)
-	{
 
 		return (-1);
-	}
 	else if (n == 0)
-	{
 
 		return (1);
-	}
 	else
-	{
-		res = n * factorial(n - 1);
-	}
-
-	return (res);
+		return (n * factorial(n - 1));
 }
