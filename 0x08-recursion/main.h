46e741e2ad3befb540c899ca8da613dd1e801341
Mike Attara <mpyebattara@gmail.com>
2022-09-28 23:18:06 +0000

    print string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+/**
+ * File: main.h
+ * Author: Mike Attara
+ * Desc: Header file for function declarations
+ */
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* HEADER_FILE*/
