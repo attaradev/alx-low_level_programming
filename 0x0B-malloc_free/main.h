47fdaf1f60d0888c8445ead91a35f0133fb3b154
Mike Attara <mpyebattara@gmail.com>
2022-10-03 20:19:16 +0000

    init files

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+/**
+ * File: main.h
+ * Author: Mike Attara
+ * Desc: Header file for function declarations
+ */
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /* HEADER_FILE*/
