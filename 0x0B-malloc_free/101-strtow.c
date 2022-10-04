22e4f352ed1468b27592843cf31784c8a1856e89
Mike Attara <mpyebattara@gmail.com>
2022-10-04 00:31:01 +0000

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -29,7 +29,7 @@ int word_count(char *str)
 	{
 		if ((str[i] != ' ') && (str[i] != '\0'))
 			++i;
-		else if (((str[i] == ' ') || (str[i] == '\0')) && (str[i - 1] != ' '))
+		else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
 		{
 			++words;
 			++i;
@@ -51,7 +51,7 @@ char **strtow(char *str)
 	char **words;
 	int i, j = 0, temp = 0, size = 0, num_word = word_count(str);
 
-	if (num_word == 0 || str == " ")
+	if (num_word == 0)
 		return (NULL);
 	words = (char **)malloc(sizeof(char *) * (num_word + 1));
 	if (words != NULL)
