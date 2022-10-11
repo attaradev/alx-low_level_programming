c1a06a117ac6f74e4fe09b9560e5ee49bd91781a
Mike Attara <mpyebattara@gmail.com>
2022-10-11 23:33:20 +0000

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - A dog structure
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner
+ *
+ * Description: A structure for a dog
+ */
+struct dog
+{
+  char *name;
+  int age;
+  char *owner;
+};
