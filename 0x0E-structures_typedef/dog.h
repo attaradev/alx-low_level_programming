cca4facecc4180077d614f99beb99a931c5217b5
Mike Attara <mpyebattara@gmail.com>
2022-10-11 23:40:02 +0000

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,15 +5,15 @@
  * struct dog - A dog structure
  * @name: Name of dog
  * @age: Age of dog
- * @owner: Name of owner
+ * @owner: Owner of dog
  *
  * Description: Data structure for a dog
  */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
