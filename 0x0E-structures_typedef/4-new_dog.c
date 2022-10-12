4babcb1c2d8e36975c31e585f5c7d19bf78daa2c
Mike Attara <mpyebattara@gmail.com>
2022-10-12 00:25:28 +0000

    refactor

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,6 @@
 #include <stdlib.h>
+#include <strings.h>
 #include "dog.h"
-
-/**
- * word_len - calculate length of given word
- * @word - given word
- * Return: length of given word
- */
-int word_len(char *word)
-{
-	int length = 0;
-
-	while (word != NULL && word[length])
-		length++;
-
-	return (length);
-}
 
 /**
  * new_dog - creates a new dog structure
@@ -26,36 +12,17 @@ int word_len(char *word)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int name_length = word_len(name);
-	int owner_length = word_len(owner);
-	int i;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
+	char *name_copy, *owner_copy;
 
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+
+	if (name == NULL || owner == NULL || new_dog == NULL)
 		return (NULL);
 
-	cpyname = malloc(name_length + 1);
-	if (cpyname == NULL)
-		return (NULL);
-
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
-
-	cpyowner = malloc(owner_length + 1);
-	if (cpyowner == NULL)
-		return (NULL);
-
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
-
-	new_dog->name = cpyname;
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
+	new_dog->name = name_copy;
 	new_dog->age = age;
-	new_dog->owner = cpyowner;
+	new_dog->owner = owner_copy;
 	return (new_dog);
 }
