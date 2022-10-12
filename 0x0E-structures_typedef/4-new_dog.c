2e921f87de0d9a1f21b760bd0227c96f5b4b5f05
Mike Attara <mpyebattara@gmail.com>
2022-10-12 01:17:09 +0000

    refactor

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,21 +11,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name_copy, *owner_copy;
-	int name_length = 0, owner_length = 0, i;
+	char *name_copy;
+	char *owner_copy;
+	int name_length = 0;
+	int owner_length = 0;
+	int i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	while (name[name_length])
 		name_length++;
 	while (owner[owner_length])
 		owner_length++;
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
 	name_copy = malloc(name_length + 1);
 	if (name_copy == NULL)
 	{
@@ -35,7 +35,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; name[i]; i++)
 		name_copy[i] = name[i];
 	name_copy[i] = '\0';
-
 	owner_copy = malloc(owner_length + 1);
 	if (owner_copy == NULL)
 	{
@@ -46,7 +45,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; owner[i]; i++)
 		owner_copy[i] = owner[i];
 	owner_copy[i] = '\0';
-
 	new_dog->name = name_copy;
 	new_dog->age = age;
 	new_dog->owner = owner_copy;
