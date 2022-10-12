1c9301e674b08f84c2886c2bce414b4ea84e3f1e
Mike Attara <mpyebattara@gmail.com>
2022-10-12 23:59:08 +0000

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,15 +10,15 @@
  **/
 void print_opcodes(char *a, int n)
 {
-  int i;
+	int i;
 
-  for (i = 0; i < n; i++)
-  {
-    printf("%.2hhx", a[i]);
-    if (i < n - 1)
-      printf(" ");
-  }
-  printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
 }
 
 /**
@@ -30,19 +30,19 @@ void print_opcodes(char *a, int n)
  */
 int main(int argc, char **argv)
 {
-  int n;
+	int n;
 
-  if (argc != 2)
-  {
-    printf("Error\n");
-    exit(1);
-  }
-  n = atoi(argv[1]);
-  if (n < 0)
-  {
-    printf("Error\n");
-    exit(2);
-  }
-  print_opcodes((char *)&main, n);
-  return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
 }
