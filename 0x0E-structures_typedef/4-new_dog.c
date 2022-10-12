a95648099c156ec14aa048db738a86e4a6a32bcc
Mike Attara <mpyebattara@gmail.com>
2022-10-12 01:15:00 +0000

    refactor

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,37 +11,44 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	char *name_copy, *owner_copy;
+	int name_length = 0, owner_length = 0, i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
+	while (name[name_length])
+		name_length++;
+	while (owner[owner_length])
+		owner_length++;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
+	name_copy = malloc(name_length + 1);
+	if (name_copy == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
+		name_copy[i] = name[i];
+	name_copy[i] = '\0';
 
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
+	owner_copy = malloc(owner_length + 1);
+	if (owner_copy == NULL)
+	{
+		free(new_dog);
+		free(name_copy);
 		return (NULL);
+	}
 	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
+		owner_copy[i] = owner[i];
+	owner_copy[i] = '\0';
 
-	new_dog->name = cpyname;
+	new_dog->name = name_copy;
 	new_dog->age = age;
-	new_dog->owner = cpyowner;
+	new_dog->owner = owner_copy;
 	return (new_dog);
 }
