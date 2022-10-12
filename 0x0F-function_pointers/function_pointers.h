a231d706a226205de3082717f7e04fd358a78a79
Mike Attara <mpyebattara@gmail.com>
2022-10-12 23:11:22 +0000

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,6 +8,7 @@
 #define HEADER_FILE
 #include <stddef.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
