e598cfcaa9022d2cb5e1e6201a57d425a3e74918
Mike Attara <mpyebattara@gmail.com>
2022-10-14 07:22:14 +0000

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,7 @@
 
 /**
  * print_all - prints all arguments
- * @format: a list of types of arguments passed to the function
+ * @format: list of types of arguments passed to the function
  * Return: void
  */
 void print_all(const char *const format, ...)
