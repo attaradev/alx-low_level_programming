937b972b4b0877ddd373e96af50bcb100f4a71fa
Mike Attara <mpyebattara@gmail.com>
2022-10-14 09:30:02 +0000

    refactor

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,16 +8,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
 
 	int sum = 0;
 	unsigned int i;
 	va_list ptr;
 
+	if (n == 0)
+		return (0);
+
 	va_start(ptr, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
+
 	va_end(ptr);
 
 	return (sum);
