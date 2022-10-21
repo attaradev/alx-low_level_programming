ca7b6ecab540812104e44bbc3b8c7d988e075fd1
Mike Attara <mpyebattara@gmail.com>
2022-10-21 01:47:10 +0000

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,25 @@
-#ifndef LISTS_HEADER
-#define LISTS_HEADER
+#ifndef LIST_HEAD
+#define LIST_HEAD
+
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+  char *str;
+  unsigned int len;
+  struct list_s *next;
+} list_t;
 
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
-#endif /* LISTS_HEADER */
+#endif /* LIST_HEAD */
